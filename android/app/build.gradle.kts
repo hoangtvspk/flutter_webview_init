97b72890bff253bbe150a0520c71ab955e932e98
hoangtvspk <hoangtv.spk@gmail.com>
2025-12-11 00:47:13 +0700

    deploy: android - fix order jobs

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode = 1
+        versionCode = 3
         versionName = "1.0.0"
         multiDexEnabled = true
     }
