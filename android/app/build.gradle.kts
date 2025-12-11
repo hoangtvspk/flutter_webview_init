ac061b63031ada1756f7113a0102caf2dce7dfc5
hoangtran153 <hoang.tran@nextchain.kr>
2025-12-11 10:14:17 +0700

    deploy: android - ignore auto update version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode = 7
+        versionCode = 8
         versionName = "1.0.0"
         multiDexEnabled = true
     }
